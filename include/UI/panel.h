2cac65d2b5d814ae44d84d9ffe467194079f8b07
Linus <linus.hasselkvist@gmail.com>
2025-04-01 11:59:53 +0200

    added panel

--- include/UI/panel.h
+++ include/UI/panel.h
@@ -1,7 +1,15 @@
 #ifndef PANEL_H
 #define PANEL_H
 
-#include "../../source/game.h"
+#include <stdbool.h>
+#include <SDL.h>
 
+typedef struct Panel *Panel;
+
+Panel UI_panelCreate();
+
+void UI_panelSetAppearance(Panel aPanel, SDL_Rect rect, SDL_Color src_bg);
+
+void UI_panelRender(SDL_Renderer *pRend, Panel aPanel);
 
 #endif
