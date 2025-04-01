025e30414d5bf67b50d6e6017b24b06f25ad5b29
SardorJ <65908115+SardorJ@users.noreply.github.com>
2025-04-01 14:11:50 +0200

    fixed tabs clientLife

--- source/clientLife.c
+++ source/clientLife.c
@@ -6,28 +6,28 @@ void startClient(ClientView *pView,ClientControl *pControl){
   pView->windowWidth = 800;
 
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-      printf("Error initializing SDL: %s\n", SDL_GetError());
+    printf("Error initializing SDL: %s\n", SDL_GetError());
   }
   if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-        SDL_Quit();
+    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+    SDL_Quit();
   }
   if (TTF_Init() == -1) {
-      printf("TTF_Init error: %s\n", TTF_GetError());
+    printf("TTF_Init error: %s\n", TTF_GetError());
   }
 
   pView->pWin = SDL_CreateWindow("main",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,pView->windowWidth,pView->windowHeight,0);
   if (!pView->pWin) {
-      printf("Error creating window: %s\n", SDL_GetError());
-      IMG_Quit();
-      SDL_Quit();
+    printf("Error creating window: %s\n", SDL_GetError());
+    IMG_Quit();
+    SDL_Quit();
   }
 
   pView->pRend = SDL_CreateRenderer(pView->pWin,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (!pView->pRend) {
-      SDL_DestroyWindow(pView->pWin);
-      IMG_Quit();
-      SDL_Quit();
+    SDL_DestroyWindow(pView->pWin);
+    IMG_Quit();
+    SDL_Quit();
   }
 
   pControl->isRunning = true;
