833e0869c89ac94801fb8571c17804eb7d2bfcf0
halmosawi12@gmail.com <halmosawi12@gmail.com>
2025-04-02 12:24:11 +0200

    Lagt till Checklista med Checkbox

--- include/UI/checklist.h
+++ include/UI/checklist.h
@@ -1,5 +1,14 @@
 #ifndef CHECKLIST_H
 #define CHECKLIST_H
 
+#include <stdbool.h>
+#include <SDL.h>
+
+typedef struct Checklist *Checklist;
+
+Checklist UI_checklistCreate();
+void UI_checklistRendrer(SDL_Renderer* pRend, Checklist achecklist);
+void UI_ChecklistAddItem(Checklist checklist, char *txt);
+void UI_ChecklistDestroy(Checklist checklist);
 
 #endif
