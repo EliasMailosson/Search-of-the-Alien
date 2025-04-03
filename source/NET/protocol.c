3caf6776f7de8c3fe19aebaf0ab5d9c9423520d7
Caspar Ekroth <caspar.ekroth@gmail.com>
2025-04-03 12:00:10 +0200

    send arra is working

--- source/NET/protocol.c
+++ source/NET/protocol.c
@@ -54,24 +54,20 @@ void NET_protocolSendString(UDPpacket *pUDPpkg,UDPsocket Socket,IPaddress IP,
     NET_stdPackageDestroy(packet);
 }
 
-void NET_protocolSendArray(UDPpacket *pUDPpkg,UDPsocket Socket,IPaddress IP,
-    GameState GS,MessageType msgType, const void* placeHolder, Uint32 arraySize){
-    
-    Uint32 payloadSize = (Uint32)sizeof(placeHolder);
-    if (!placeHolder || payloadSize == 0) {
+void NET_protocolSendArray(UDPpacket *pUDPpkg, UDPsocket Socket, IPaddress IP,
+                           GameState GS, MessageType msgType, const void* array, Uint32 arraySize){
+    Uint32 payloadSize = arraySize;
+    if (!array || payloadSize == 0) {
         printf("Invalid array data.\n");
         return;
     }
     
-    StdPackage packet = NET_stdPackageCreate(GS,msgType,payloadSize);
+    StdPackage packet = NET_stdPackageCreate(GS, msgType, payloadSize);
     if (!packet) {
         printf("Failed to create dynamic packet.\n");
         return;
     }
-
-    NET_stdPackagePayloadArray(packet, placeHolder, arraySize);
-    //NET_stdPackageWrite32Payload(packet,(Uint32*)placeHolder);
-    // Serialize the packet.
+    NET_stdPackagePayloadArray(packet, array, arraySize);
     Uint8 *sBuffer = NULL;
     Uint32 serializedSize = NET_stdPackageSerialize(packet, &sBuffer);
     if (serializedSize == 0 || !sBuffer) {
@@ -84,7 +80,7 @@ void NET_protocolSendArray(UDPpacket *pUDPpkg,UDPsocket Socket,IPaddress IP,
     pUDPpkg->address = IP;
     // Send the UDP packet.
     SDLNet_UDP_Send(Socket, -1, pUDPpkg);
-    //free
+    // Clean up.
     free(sBuffer);
     NET_stdPackageDestroy(packet);
 }
