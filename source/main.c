1ebe727bd684fb1c43387de87059aaf65f921227
Elias M <elias.mailosson@gmail.com>
2025-04-04 11:11:06 +0200

    Added button hover effect

--- source/main.c
+++ source/main.c
@@ -10,21 +10,21 @@ int main(int argc, char **argv ){
     ClientControl control = {0};
 
     startClient(&view, &control);
-    //gameLoop(&control, &view);
-    Client aClient = {0};
-    aClient = NET_clientCreate();
-    NET_clientConnect(aClient);
+    gameLoop(&control, &view);
+    // Client aClient = {0};
+    // aClient = NET_clientCreate();
+    // NET_clientConnect(aClient);
     //NET_clientSend(aClient);
     //NET_clientSendString(aClient, MENU, LOBBY_LIST, "Elias Mailadasson");
     //PlayerList placeholder[3] = {{.ID = 6543, .pos.x = 12, .pos.y = 55}, {.ID = 1235, .pos.x = 52, .pos.y = 35}, {.ID = 6435, .pos.x = 1, .pos.y = 2}};
     //NET_clientSendArray(aClient, MENU, LOBBY_LIST_RESPONSE, placeholder, sizeof(placeholder));
-    NET_clientSendString(aClient,MENU,CONNECT,"Caspar");
-    int startTime = SDL_GetTicks();
-    while (SDL_GetTicks() - startTime < 5000 ){
-        NET_clientReceiver(aClient);
-    }
+    // NET_clientSendString(aClient,MENU,CONNECT,"Caspar");
+    // int startTime = SDL_GetTicks();
+    // while (SDL_GetTicks() - startTime < 5000 ){
+    //     NET_clientReceiver(aClient);
+    // }
     
-    NET_clientDestroy(aClient);
+    // NET_clientDestroy(aClient);
     killClient(&view);
     return 0;
 }
