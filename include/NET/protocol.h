b535daa90d8fb57f8d266a6b705d4846ea3b5d9a
Caspar Ekroth <caspar.ekroth@gmail.com>
2025-04-04 11:03:44 +0200

    added PlayerListRM and PlayerListAdd this will be moved to shared

--- include/NET/protocol.h
+++ include/NET/protocol.h
@@ -12,4 +12,17 @@ void NET_protocolSendString(UDPpacket *pUDPpkg,UDPsocket Socket,IPaddress IP,
 void NET_protocolSendArray(UDPpacket *pUDPpkg,UDPsocket Socket,IPaddress IP,
                             GameState GS,MessageType msgType,const void* array, Uint32 arraySize);
 
+//########## ska ligga i shaerde #########
+
+typedef struct{
+    int ID;
+    SDL_Point pos;
+    GameState state;
+}PlayerList1;
+
+
+void NET_PlayerListRemovPlayer1(PlayerList1 **list,int index, int *listCount);
+void NET_PlayerListAddPlayer1(PlayerList1 **list,PlayerList1 newPlayer,int *listCount);
+
+
 #endif
