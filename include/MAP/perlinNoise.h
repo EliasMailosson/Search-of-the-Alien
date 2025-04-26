c42e6bb8cb8af3b2e2b6140c7eb5c5c687598d42
Caspar Ekroth <caspar.ekroth@gmail.com>
2025-04-26 10:59:39 +0200

    2d perlin noise is working

---
+++ include/MAP/perlinNoise.h
@@ -0,0 +1,15 @@
+#ifndef PERLIN_NOISE_H
+#define PERLIN_NOISE_H
+//#include "../../include/MAP/map.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>  
+#include <time.h>
+
+#define MAP_HEIGHT 30
+#define MAP_WIDTH 30
+
+void MAP_generat1DNoise(float *output, int size);
+
+
+#endif
--- source/MAP/perlinNoise.c
+++ source/MAP/perlinNoise.c
@@ -1,6 +1,7 @@
 #include "../../include/MAP/perlinNoise.h"
 
 static void MAP_perlinNoise1D(int count, float *fSeed, int octaves,float bias, float *output);
+static void MAP_perlinNoise2D(int width, int height, float *fSeed, int octaves,float bias, float *output);
 
 
 void MAP_generatePerlinNoise(){
@@ -8,10 +9,10 @@ void MAP_generatePerlinNoise(){
 }
 
 void MAP_generate2DNoise(float *output,int outputHeight, int outputWidth){
-    float *fNoiseSeed2D = {0};
-    float *perlinNoise2D = {0};
+    float *fNoiseSeed2D = malloc((outputHeight * outputWidth) * sizeof *fNoiseSeed2D);
     for (int i = 0; i < outputHeight * outputWidth; i++) fNoiseSeed2D[i] = (float)rand() / (float)RAND_MAX;
-
+    MAP_perlinNoise2D(outputWidth, outputHeight, fNoiseSeed2D, 3, 2.0f, output);
+    free(fNoiseSeed2D);
 }
 
 
@@ -45,12 +46,45 @@ static void MAP_perlinNoise1D(int count, float *fSeed, int octaves,float bias, f
     
 }
 
+static void MAP_perlinNoise2D(int width, int height, float *fSeed, int octaves,float bias, float *output){
+    for (int x = 0; x < width; x++){
+        for(int y = 0; y < height; y++){
+            float noise = 0.0f;
+            float scale = 1.0f;
+            float scaleAcc = 0.0f;
+
+            for (int j = 0; j < octaves; j++){
+                int pitch = width >> 2;
+                if (pitch == 0) continue; 
+                int sampleX1 = (x / pitch) * pitch;
+                int sampleY1 = (y / pitch) * pitch;
+
+                int sampleX2 = (sampleX1 + pitch) % width;
+                int sampleY2 = (sampleY1 + pitch) % width;
+
+                float blendX = (float)(x - sampleX1) / (float)pitch;
+                float blendY = (float)(y - sampleY1) / (float)pitch;
+                //linier interpalation
+                float sampleT = (1.0f - blendX) * fSeed[sampleY1 * width + sampleX1] + blendX * fSeed[sampleY1 * width + sampleX2];
+                float sampleB = (1.0f - blendX) * fSeed[sampleY2 * width + sampleX1] + blendX * fSeed[sampleY2 * width + sampleX2];
+                noise += (blendY * (sampleB - sampleT) + sampleT) * scale;
+                scaleAcc += scale;
+                scale = scale/ bias;
+            }
+            output[y * width + x] = noise / scaleAcc;
+        }
+    }
+    
+}
+
 // int main(){
 //     srand(time(NULL));
-//     float output[MAP_HEIGHT] = {0};
-//     MAP_generat1DNoise(output,MAP_HEIGHT);
-//     for (int i = 0; i < MAP_HEIGHT; i++){
-//         printf("%.1f ",output[i]*3);
+//     float output[MAP_HEIGHT * MAP_WIDTH] = {0};
+//     //MAP_generat1DNoise(output,MAP_HEIGHT);
+//     MAP_generate2DNoise(output,MAP_HEIGHT,MAP_WIDTH);
+//     for (int i = 0; i < MAP_HEIGHT * MAP_WIDTH; i++){
+//         if(i%30 == 0) printf("\n");
+//         printf("%d,",(int)roundf(output[i]*10));
 //     }
 //     return 0;
 // }
