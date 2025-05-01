c44b908fbc4070b109f09c067be9e37ca0a43dfa
Caspar Ekroth <caspar.ekroth@gmail.com>
2025-05-01 12:52:19 +0200

    canges for windows

--- source/CONCURRENCY/threads.c
+++ source/CONCURRENCY/threads.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 
 #ifdef _WIN32
+#include <windows.h>
+
+typedef struct {
+    thread_func_t func;
+    void* arg;
+} thread_args_t;
 
 static DWORD WINAPI windows_thread_func(LPVOID arg) {
-    thread_func_t* real_func = ((thread_func_t*)arg)[0];
-    void* real_arg = ((void**)arg)[1];
-    free(arg);
-    real_func(real_arg);
-    return 0;
+    thread_args_t* args = (thread_args_t*)arg;
+    thread_func_t real_func = args->func;
+    void* real_arg = args->arg;
+    free(args);
+    void* result = real_func(real_arg);
+    return (DWORD)(uintptr_t)result;
 }
 
 int thread_create(thread_t* thread, thread_func_t func, void* arg) {
-    void** args = malloc(2 * sizeof(void*));
+    thread_args_t* args = malloc(sizeof(thread_args_t));
     if (!args) return -1;
-    args[0] = (void*)func;
-    args[1] = arg;
+    args->func = func;
+    args->arg  = arg;
     *thread = CreateThread(NULL, 0, windows_thread_func, args, 0, NULL);
     return *thread != NULL ? 0 : -1;
 }
@@ -46,7 +53,7 @@ int mutex_destroy(mutex_t* mutex) {
     return 0;
 }
 
-#else // POSIX
+#else  // POSIX
 
 int thread_create(thread_t* thread, thread_func_t func, void* arg) {
     return pthread_create(thread, NULL, func, arg);
